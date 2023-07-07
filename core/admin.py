e56aa32370230e52c6738b09f750f0c1c5fe45a6
buzov <abuzov@list.ru>
2023-07-07 17:46:47 +0300

    Deploy

--- core/admin.py
+++ core/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 
 from core.models import User
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(UserAdmin):
     empty_value_display = '-empty-'
-    exclude = ('password',)
+    # exclude = ('password',)
     list_display = ('username', 'email', 'first_name', 'last_name',)
     list_filter = ('is_staff', 'is_active', 'is_superuser',)
     search_fields = ['first_name', 'last_name', 'username']
     readonly_fields = ('last_login', 'date_joined',)
+
+
+admin.site.unregister(Group)
